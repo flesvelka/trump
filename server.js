5341e3541f56b082dab9385eb11163b2e6457534
hirano <s10166ny@edu.tuis.ac.jp>
2013-06-18 16:36:32 +0900

    server.js更新しました

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/*
 var fs = require('fs');
 var app = require('http').createServer( function (req, res) {
     fs.readFile(__dirname + '/game.html', function (err, data) {
@@ -17,7 +16,6 @@ io.sockets.on('connection', function (socket) {
 });
 
 app.listen(80);
-*/
 
 var app = require('express').createServer()
   , io = require('socket.io').listen(app);
@@ -31,28 +29,3 @@ io.configure('production', function(){
 });
  
 app.listen(80);
- 
-// '/'にアクセスがきたらindex.htmlを返す
-// こういう部分は普通apacheとかでやらせる？
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
-});
- 
-io.sockets.on('connection', function(socket){
- 
-  // 接続直後にクライアントにメッセージ送る
-  socket.emit('news', { msg: 'hello world from server'});
- 
-  // 上記メッセージを受け取ったクライアントの返事を受け取る
-  // このへんは単なるテスト。
-  socket.on('resp', function(resp){
-    console.log(resp.resp);
-  });
- 
-  // クライアントが何か言った(say)ら、ブロードキャストする
-  socket.on('say', function(msg){
-    console.log('said: ' + msg.msg);
-    socket.broadcast.emit('said', {msg: msg.msg});
-  });
- 
-});
